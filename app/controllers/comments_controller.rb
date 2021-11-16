944c5310bd0b1ba63ddf071bdc791f16a3119a99
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-11-16 18:15:17 +0900

    コメントの保存に失敗すると起きるエラーの解消

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
       else
          @prototype = Prototype.find(comment.prototype_id)
          @comment = Comment.new
+         @comments = @prototype.comments.includes(:user)
          render "prototypes/show"
       end
    end
